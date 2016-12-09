b1107639dafd9483424cd2a4f33856c6169550b1
David Mar Gunnarsson <dmrgunnarsson@gmail.com>
2016-12-09 21:27:38 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WhoComponent } from './who/who.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ContactComponent } from './contact/contact.component';
+import { WhatComponent } from './what/what.component';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { RegisterComponent } from './register/register.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    WhoComponent,
+    LogInComponent,
+    ContactComponent,
+    WhatComponent,
+    HomeComponent,
+    BrowseComponent,
+    RegisterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
