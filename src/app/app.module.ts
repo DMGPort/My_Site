a3185e0695401a91eecf3d47c2b7b20510efee52
David Mar Gunnarsson <dmrgunnarsson@gmail.com>
2016-12-10 15:41:36 +0000

    burger-menu added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './home/home.component';
 import { BrowseComponent } from './browse/browse.component';
 import { RegisterComponent } from './register/register.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { CarouselComponent } from './carousel/carousel.component';
     HomeComponent,
     BrowseComponent,
     RegisterComponent,
-    CarouselComponent
+    CarouselComponent,
+    BurgerMenuComponent
   ],
   imports: [
     BrowserModule,
